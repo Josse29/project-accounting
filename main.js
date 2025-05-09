871d9b1757a4813586b02fc1ecf0cfb8ae5ce266
Josse29 <pinemjosse@gmail.com>
2025-05-09 17:15:23 +0700

    updating

--- main.js
+++ main.js
@@ -38,7 +38,8 @@ function createWindow() {
   }
   // db
   const userDataPath = app.getPath("userData");
-  // console.log(userDataPath); C:\Users\ASUS\AppData\Roaming\josstack
+  // C:\Users\ASUS\AppData\Roaming\josstack
+  // username : josse112, password : billionaireWFH100%
   const dbDestination = path.join(userDataPath, "myapps.db");
   const db = new sqlite3.Database(dbDestination);
   User(ipcMain, db, bcryptjs, jwt);
